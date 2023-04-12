930a331dd324b4c61989b04f6a21c20594fe7d7a
nelly476 <nellybulatova12@gmail.com>
2023-04-12 10:12:51 +0300

    modify initTaskElemBtn fn

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -69,18 +69,60 @@ const UI = (() => {
   }
 
   function initTaskElemBtn(id) {
-    document.getElementById(`${id}`).addEventListener("click", () => {
+    document.getElementById(`delete-${id}`).addEventListener("click", () => {
       task.deleteTask(id);
-      removeTaskFromTasksArea();
+      removeTasksFromTasksArea();
       // console.log(task.toDoCollection);
       showExistingTasks();
     });
+
+    document.getElementById(`info-${id}`).addEventListener("click", () => {
+      const target = task.toDoCollection.filter((item) => item.id === id);
+      console.log(target);
+    });
   }
 
-  function removeTaskFromTasksArea() {
-    const allTasksArea = document.querySelector(".all-tasks");
-    allTasksArea.innerHTML = "";
-  }
+  // function shoeDetailsModule() {
+  //   const allTasks = document.querySelector(".all-tasks");
+
+  //     const detailsModule = document.createElement("div");
+  //     toDoModule.className = "to-do-module";
+  //     toDoModule.innerHTML = `
+  //     <h2>Add a new task</h2>
+  //     <div class="newTaskForm">
+  //       <div class="newNoteTitle-area">
+  //         <label for="title">Title </label>
+  //         <input type="text" name="title" id="title" required />
+  //       </div>
+  //       <div class="newNoteDescription-area">
+  //         <label for="description">Description </label>
+  //         <textarea id="description" name="description" rows="6" cols="33"></textarea>
+  //       </div>
+  //       <div class="deadline-area">
+  //       <label for="deadline-date">Deadline</label>
+  //       <input type="date" id="deadline-date" name="deadline-date">
+  //       </div>
+  //       <div class="priority-area">
+  //       <p>Priority:</p>
+  //       <p class="low-priority-btn">LOW</p>
+  //       <p class="medium-priority-btn">MEDIUM</p>
+  //       <p class="high-priority-btn">HIGH</p></div>
+  //       <div class="form-btn-area">
+  //         <button class="close-module-button">Cancel</button>
+  //         <input class="add-module-button" type="submit" value="Add" />
+  //       </div>
+  //     </div>
+  //     </div>
+  //     `;
+  //     content.appendChild(toDoModule);
+  //     initModuleBtn();
+
+  // }
+
+  // function removeTasksFromTasksArea() {
+  //   const allTasksArea = document.querySelector(".all-tasks");
+  //   allTasksArea.innerHTML = "";
+  // }
 
   function createTaskElement(item) {
     const allTasksArea = document.querySelector(".all-tasks");
@@ -95,9 +137,9 @@ const UI = (() => {
     </span>
      <span class="right-side-task-svg">
      <p>${item.dueDate}</p>
-     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M48 80a48 48 0 1 1 96 0A48 48 0 1 1 48 80zM0 224c0-17.7 14.3-32 32-32H96c17.7 0 32 14.3 32 32V448h32c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H64V256H32c-17.7 0-32-14.3-32-32z"/></svg>
+     <svg id=info-${item.id} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M48 80a48 48 0 1 1 96 0A48 48 0 1 1 48 80zM0 224c0-17.7 14.3-32 32-32H96c17.7 0 32 14.3 32 32V448h32c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H64V256H32c-17.7 0-32-14.3-32-32z"/></svg>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>
-      <svg id=${item.id} class="delete-btn" id=${item.id} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M290.7 57.4L57.4 290.7c-25 25-25 65.5 0 90.5l80 80c12 12 28.3 18.7 45.3 18.7H288h9.4H512c17.7 0 32-14.3 32-32s-14.3-32-32-32H387.9L518.6 285.3c25-25 25-65.5 0-90.5L381.3 57.4c-25-25-65.5-25-90.5 0zM297.4 416H288l-105.4 0-80-80L227.3 211.3 364.7 348.7 297.4 416z"/></svg>
+      <svg id=delete-${item.id} class="delete-btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M290.7 57.4L57.4 290.7c-25 25-25 65.5 0 90.5l80 80c12 12 28.3 18.7 45.3 18.7H288h9.4H512c17.7 0 32-14.3 32-32s-14.3-32-32-32H387.9L518.6 285.3c25-25 25-65.5 0-90.5L381.3 57.4c-25-25-65.5-25-90.5 0zM297.4 416H288l-105.4 0-80-80L227.3 211.3 364.7 348.7 297.4 416z"/></svg>
     </span>
      `;
     allTasksArea.appendChild(taskElement);
